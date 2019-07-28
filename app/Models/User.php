e9499e951c0cb1735a0339e8c255428097f79949
chen <mdoudouchen@gmail.com>
2019-07-28 06:25:16 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,6 +48,52 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
+    public function follwers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    /**
+     * 关注
+     * @param $user_ids
+     */
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->sync($user_ids, false);
+    }
+
+    /**
+     * 取消关注
+     * @param $user_ids
+     */
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->detach($user_ids);
+    }
+
+    /**
+     * 是否关注
+     * @param $user_id
+     * @return mixed
+     */
+    public function isFollowing($user_id)
+    {
+        return $this->followings->cntains($user_id);
+    }
+
     /**
      * The attributes that should be cast to native types.
      *
