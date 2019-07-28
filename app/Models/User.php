65f49193f7252ad69e2573b2b309328128db1ce3
chen <mdoudouchen@gmail.com>
2019-07-28 09:38:20 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,7 +40,11 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at', 'desc');
+        $user_ids = $this->followers->pluck('id')->toarray();
+        array_push($user_ids, $this->id);
+
+//        return $this->statuses()->orderBy('created_at', 'desc');
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderBy('created_at', 'desc');
     }
 
     public function statuses()
